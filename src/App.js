47018102e02192d591dc172ed26f3334a8be293c
Shobika <shobipri57@gmail.com>
2025-03-17 14:51:12 +0530

    lc4

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
 import Cinematica from "./components/Events/Cinematica";
 import FunFinity from "./components/Events/FunFinity";
 import SharkTank from "./components/Events/SharkTank";
@@ -11,18 +12,19 @@ import Main from "./Main";
 function App() {
   return (
     <Router>
-<Routes>
-          <Route path="/" element={<Main/>} />
-          <Route path="events/Presentation-BlitZ" element={<PaperPresentation />} />
-          <Route path="events/CodeRescue" element={<CodeRescue />} />
-          <Route path="events/WebWhiz" element={<WebWhiz />} />
-          <Route path="events/Cinematica" element={<Cinematica />} />
-          <Route path="events/FunFinity-Wars" element={<FunFinity />} />
-          <Route path="events/SharK-Tank" element={<SharkTank />} />
-        </Routes>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="events/Presentation-BlitZ" element={<PaperPresentation />} />
+        <Route path="events/CodeRescue" element={<CodeRescue />} />
+        <Route path="events/WebWhiz" element={<WebWhiz />} />
+        <Route path="events/Cinematica" element={<Cinematica />} />
+        <Route path="events/FunFinity-Wars" element={<FunFinity />} />
+        <Route path="events/SharK-Tank" element={<SharkTank />} />
+      </Routes>
 
     </Router>
-        
+
   );
 }
 
